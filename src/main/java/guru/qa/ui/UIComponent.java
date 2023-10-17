8de208b83d08156cd5eaf086d251011ad2a6f430
dtuchs <dtuchs@gmail.com>
2023-10-18 00:38:32 +0600

    first commit

---
+++ src/main/java/guru/qa/ui/UIComponent.java
@@ -0,0 +1,16 @@
+package guru.qa.ui;
+
+import guru.qa.service.Session;
+
+public interface UIComponent {
+
+    Session render(Session session);
+
+    class MockUIComponent implements UIComponent {
+        @Override
+        public Session render(Session session) {
+            System.out.println("MockUIComponent!");
+            return session;
+        }
+    }
+}
